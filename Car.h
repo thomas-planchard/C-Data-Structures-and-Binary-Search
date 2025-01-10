8f670b3a6cfe31efec1aa096d2a4ea8fb6438b75
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2025-01-10 19:12:37 +0100

    Car and Parking class

---
+++ Car.h
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Car
+{
+    public:
+        string make;
+        string model;
+        int year;
+        string color;
+        Car();
+        Car(string make, string model, int year, string color);
+        ~Car();
+        void setMake(string make);
+        void setModel(string model);
+        void setYear(int year);
+        void setColor(string color);
+        void setCar(string make, string model, int year, string color);
+        string getMake();
+        string getModel();
+        int getYear();
+        string getColor();
+        void display();
+};
+
+Car::Car()
+{
+    make = "";
+    model = "";
+    year = 0;
+    color = "";
+}  
+
+Car::Car(string make, string model, int year, string color)
+{
+    this->make = make;
+    this->model = model;
+    this->year = year;
+    this->color = color;
+}
+
+Car::~Car()
+{
+    cout << "This car has an engine problem and no longer works." << endl;
+}
+
+void Car::setMake(string make)
+{
+    this->make = make;
+}
+
+void Car::setModel(string model)
+{
+    this->model = model;
+}
+
+void Car::setYear(int year)
+{
+    this->year = year;
+}
+
+
+void Car::setColor(string color)
+{
+    this->color = color;
+}
+
+void Car::setCar(string make, string model, int year, string color)
+{
+    this->make = make;
+    this->model = model;
+    this->year = year;
+    this->color = color;
+}
+
+string Car::getMake()
+{
+    return make;
+}
+
+string Car::getModel()
+{
+    return model;
+}
+
+int Car::getYear()
+{
+    return year;
+}
+
+string Car::getColor()
+{
+    return color;
+}
+
+void Car::display()
+{
+    cout << "Make: " << make << endl;
+    cout << "Model: " << model << endl;
+    cout << "Year: " << year << endl;
+    cout << "Color: " << color << endl;
+}
---
+++ Parking.h
@@ -0,0 +1,127 @@
+#include "Car.h"
+
+
+class Parking : public Car
+{
+
+    public:
+        vector<Car*> cars;
+        Parking();
+        ~Parking();
+        void parkCar(Car* car);
+        void removeCar(Car* car);
+        void displayCars();
+        void sortCars(vector<Car>* car, int start, int end);
+        void searchCar(string make, string model, int year, string color);
+
+    private:
+        void merge(vector <Car>* car, int start, int mid, int end);
+
+};
+
+
+Parking::Parking()
+{
+    cars = {};
+}
+
+Parking::~Parking()
+{
+    cout << "The parking lot is now empty." << endl;
+}
+
+void Parking::parkCar(Car* car)
+{
+    cars.push_back(car);
+}
+
+void Parking::removeCar(Car* car)
+{
+    for (int i = 0; i < cars.size(); i++)
+    {
+        if (cars[i] == car)
+        {
+            cars.erase(cars.begin() + i);
+            break;
+        }
+    }
+}
+
+
+void Parking::displayCars()
+{
+    for (int i = 0; i < cars.size(); i++)
+    {
+        cars[i]->display();
+    }
+}
+
+
+void Parking::sortCars(vector<Car>* cars, int start, int end)
+{
+    if(start < end)
+    {
+        int mid = start + (end - start) / 2;
+        sortCars(cars, start, mid);
+        sortCars(cars, mid + 1, end);
+        merge(cars, start, mid, end);
+    }
+    else return;
+}
+
+void Parking::merge(vector<Car>* cars, int start, int mid, int end)
+{
+    int sizeLeft = mid - start + 1;
+    int sizeRight = end - mid;
+    vector<Car> left(sizeLeft);
+    vector<Car> right(sizeRight);
+    for (int i = 0; i < sizeLeft; i++)
+    { 
+        left[i] = (*cars)[start + i];
+    }
+    for (int i = 0; i < sizeRight; i++)
+    {
+        right[i] = (*cars)[mid + 1 + i];
+    }
+    int i = 0;
+    int j = 0;
+    int k = start;
+    while (i < sizeLeft && j < sizeRight)
+    {
+        if (left[i].getYear() <= right[j].getYear())
+        {
+            (*cars)[k] = left[i];
+            i++;
+        }
+        else
+        {
+            (*cars)[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < sizeLeft)
+    {
+        (*cars)[k] = left[i];
+        i++;
+        k++;
+    }
+    while (j < sizeRight)
+    {
+        (*cars)[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+void Parking::searchCar(string make, string model, int year, string color)
+{
+    for (int i = 0; i < cars.size(); i++)
+    {
+        if (cars[i]->getMake() == make && cars[i]->getModel() == model && cars[i]->getYear() == year && cars[i]->getColor() == color)
+        {
+            cars[i]->display();
+            break;
+        }
+    }
+}
